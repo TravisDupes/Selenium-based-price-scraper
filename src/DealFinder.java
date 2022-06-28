51a0c720ef801727de23d9d812b5a3fff583301a
Travis <trmail25@gmail.com>
2022-06-28 13:25:34 -0400

    addded open box and refurbished filtering

--- src/DealFinder.java
+++ src/DealFinder.java
@@ -1,9 +1,41 @@
 
 public class DealFinder {
+
+	
+	
 	public static void main(String[] args) 
 	{
-		NeweggSiteScraper Newegg = new NeweggSiteScraper("videocard");
+		
+		boolean allowRefurbished = true;
+		boolean allowOpenBox = true;
+		boolean enforceStringMatch = false;
+			
+		String searchTerm = args[args.length - 1].toLowerCase();
+		for(int i=0; i<(args.length-1); i++)
+		{
+			if(args[i].equals("--allowrefurbished=f"))
+			{
+				allowRefurbished = false;
+			}
+			if(args[i].equals("--allowrefurbished=t"))
+			{
+				allowRefurbished = true;
+			}
+			if(args[i].equals("--allowopenbox=t"))
+			{
+				allowOpenBox = true;
+			}
+			if(args[i].equals("--allowopenbox=f"))
+			{
+				allowOpenBox = false;
+			}
+		}
+		
+		
+		NeweggSiteScraper Newegg = new NeweggSiteScraper(searchTerm);
 		Newegg.run();
+		
+		
 		while(Newegg.done == false)
 		{
 			try {
@@ -14,12 +46,25 @@ public class DealFinder {
 			}
 		}
 		
+		
+		
 		for(ItemListing item: Newegg.Items)
 		{
+			if(item.productTitle.toLowerCase().contains(searchTerm)|| enforceStringMatch == false)
+			{
+				if(allowRefurbished == true || item.isRefurbished == false)
+				{
+					if(allowOpenBox == true || item.isRefurbished == false)
+					{
 
-			System.out.println(item.seller + ":");
-			System.out.println(item.productTitle);
-			System.out.println("$" + item.priceDollars + "." +item.priceCents);
+						System.out.println(item.seller + ":");
+						System.out.println(item.productTitle);
+						System.out.println("$" + item.priceDollars + "." +item.priceCents);
+					}
+				}
+			}
+
+			
 		}
 	}
 
